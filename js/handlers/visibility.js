5bda1c948bf7c0dd773ad8b22fd0374d45b13664
M Stevens <zarek3333@gmail.com>
2021-02-22 16:35:15 -0500

    Added "_isHidden" and fix 4 narrative load issue

--- js/handlers/visibility.js
+++ js/handlers/visibility.js
@@ -20,6 +20,7 @@ define([
       this.listenTo(Adapt, {
         "trickle:steplock": this.onStepLock,
         "trickle:visibility": this.onVisibility,
+        "trickle:hidden": this.onHidden,
         "trickle:stepunlock": this.onStepUnlock,
         "trickle:kill": this.onKill,
         "trickle:finished": this.onFinished,
@@ -32,6 +33,10 @@ define([
       this.isStepLocking = true;
       this.trickleModel = view.model;
       Adapt.trigger("trickle:visibility");
+      window.setTimeout(function(){
+        Adapt.trigger("trickle:hidden");
+      }, 1340);
+      
     },
 
     onVisibility: function() {
@@ -49,7 +54,7 @@ define([
       });
 
       descendantsParentFirst.forEach(function(descendant, index) {
-        var components = descendant.findDescendantModels("components");
+        var components = descendant.findDescendantModels("component");
         if (index <= atIndex) {
           descendant.set("_isVisible", true, {pluginName:"trickle"});
           components.forEach(function(componentModel) {
@@ -78,6 +83,50 @@ define([
 
     },
 
+    onHidden: function() {
+      if (!this.isStepLocking) return;
+
+      if (!Adapt.trickle.pageView) return;
+
+      var descendantsParentFirst = Adapt.trickle.pageView.descendantsParentFirst;
+
+      var trickleModelId = this.trickleModel.get("_id");
+      var trickleType = this.trickleModel.get("_type");
+
+      var atIndex = _.findIndex(descendantsParentFirst, function(descendant) {
+        if (descendant.get("_id") === trickleModelId) return true;
+      });
+
+      descendantsParentFirst.forEach(function(descendant, index) {
+        var blocks = descendant.findDescendantModels("block");
+        if (index <= atIndex) {
+          descendant.set("_isHidden", false, {pluginName:"trickle"});
+          blocks.forEach(function(blockModel) {
+            blockModel.set("_isHidden", false, {pluginName:"trickle"});
+          });
+          return;
+        }
+
+        if (trickleType === "article" && descendant.get("_type") === "block") {
+          // make sure article blocks are shown
+          if (descendant.get("_parentId") === trickleModelId) {
+            descendant.set("_isHidden", false, {pluginName:"trickle"});
+            blocks.forEach(function(blockModel) {
+              blockModel.set("_isHidden", false, {pluginName:"trickle"});
+            });
+            return;
+          }
+        }
+
+        descendant.set("_isHidden", true, {pluginName:"trickle"});
+        blocks.forEach(function(blockModel) {
+          blockModel.set("_isHidden", true, {pluginName:"trickle"});
+        });
+
+      });
+
+    },
+
     onStepUnlock: function(view) {
       this.isStepLocking = false;
       this.trickleModel = null;
@@ -93,7 +142,12 @@ define([
       var descendantsParentFirst = Adapt.trickle.pageView.descendantsParentFirst;
       descendantsParentFirst.forEach(function(descendant) {
         descendant.set("_isVisible", true, {pluginName:"trickle"});
-        var components = descendant.findDescendantModels("components");
+        descendant.set("_isHidden", false, {pluginName:"trickle"});
+        var components = descendant.findDescendantModels("component");
+        var blocks = descendant.findDescendantModels("block");
+        blocks.forEach(function(blockModel) {
+            blockModel.set("_isHidden", false, {pluginName:"trickle"});
+        });
         components.forEach(function(componentModel) {
           componentModel.set("_isVisible", true, {pluginName:"trickle"});
         });
